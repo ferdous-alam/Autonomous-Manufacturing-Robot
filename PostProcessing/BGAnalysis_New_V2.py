b2a403bf493557bbf4828e0ffa14aa9b6e31aaec
duong-101 <85757355+duong-101@users.noreply.github.com>
2021-06-14 14:52:30 -0400

    Update BGAnalysis_New_V2.py

--- PostProcessing/BGAnalysis_New_V2.py
+++ PostProcessing/BGAnalysis_New_V2.py
@@ -1,17 +1,18 @@
 import control as control
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from scipy.fftpack import fft
+from numpy import genfromtxt
 
 plt.close('all')
 
 
 # This code is used for FFT, Data collection
-A = pd.read_csv('DOE_avg_reference_signal_timedomain.csv')  # Reference signal
-B = pd.read_csv('DOE_Ref_SampleA_300V_0dB_50avg_freq-2250kHz_location-7_Block-1_Jan-19-2020_11-18.csv')  # actual signal
+A = genfromtxt('DOE_avg_reference_signal_timedomain.csv')  # Reference signal
+B = genfromtxt('DOE_Ref_SampleA_300V_0dB_50avg_freq-2250kHz_location-7_Block-1_Jan-19-2020_11-18.csv')  # actual signal
 
-# signal range in time domain. This is lower and upper values are determined by ploting the time domain data to
+
+# signal range in time domain. This is lower and upper values are determined by plotting the time domain data to
 # determine where the pulse starts and ends
 SigRangeA_lower = 550
 SigRangeA_upper = 1050
@@ -19,7 +20,7 @@ SigRangeA_upper = 1050
 SigRangeB_lower = 800
 SigRangeB_upper = 2040
 
-Sampling_frequency = 50000000  # Sampling Frequency (MHz): 50; 25; 12.5; 6.25
+Sampling_frequency = 50*10**6  # Sampling Frequency (MHz): 50; 25; 12.5; 6.25
 
 n = len(A)+1       # number of data points !for some reason len function is returning 3999 instead of 4000
 Total_time = n/Sampling_frequency  # total time
@@ -27,27 +28,25 @@ dt = Total_time/n               # difference in time for plotting
 
 # time for measurements of A and B csv files, goes to n-1 bc at n it repeats !check this
 index = 0
-t = [None]*(n-1)
+t = [None]*(n)
 while index < n:
-    t.append(dt*index*10**6)
+    t[index] = (dt*index*10**6)
     index += 1
 
 # calculating FFT
-
 df = 1/dt  # difference between frequency
 
 # set mean value before and after pulse, eliminates noise and reflections for A and B
-No_Reflections_A = [np.mean(A)]*n  # array of mean A
-No_Reflections_A[SigRangeA_lower:SigRangeA_upper] = A[SigRangeA_lower:SigRangeA_upper]  # adds the pulse
+No_Reflections_A= [np.mean(A)]*n  # array of mean A and pulse
+No_Reflections_A[SigRangeA_lower:SigRangeB_upper] = A[SigRangeA_lower:SigRangeA_upper]
 
 No_Reflections_B = [np.mean(B)]*n  # array of mean B
 No_Reflections_B[SigRangeB_lower:SigRangeB_upper] = B[SigRangeB_lower:SigRangeB_upper]  # adds the pulse
 
-
 # take fft A
 fft_A = abs(fft(No_Reflections_A))
 index = int(n/2)
-while len(fft_A) != (n/2):
+while len(fft_A) != index:
     fft_A = np.delete(fft_A, index)
 
 # create frequency depth points for plotting
@@ -57,7 +56,7 @@ while index < (int(n/2)):
     frq_A_points[index] = index * df/n * 10**-6
     index += 1
 
-# normalize coefficents of fft of A
+# normalize coefficients of fft of A
 index = 0
 fft_A_normalized = [None]*len(fft_A)
 fft_A_max = max(fft_A[2:n])
@@ -74,16 +73,15 @@ while index < len(fft_A_normalized):
     index += 1
 index = len(fft_A_normalized)-1
 while index > 1:
-    if fft_A_normalized[index] >=.06:s
+    if fft_A_normalized[index] >= .06:
         fANindex_E = index
         break
     index -= 1
 
-
 # take fft B
 fft_B = abs(fft(No_Reflections_B))
 index = int(n/2)
-while len(fft_B) != (n/2):
+while len(fft_B) != index:
     fft_B = np.delete(fft_B, index)
 
 # create frequency depth points for plotting
@@ -101,7 +99,7 @@ while index < int(n/2):
     fft_B_normalized[index] = fft_B[index]/fft_B_max
     index += 1
 
-#  plotting fft ISNT WORKING
+#  plotting fft ISN'T WORKING
 plt.plot(frq_A_points, abs(fft_A))
 plt.plot(frq_B_points, abs(fft_B))
 plt.xlabel('Frequency [MHz]')
@@ -126,7 +124,7 @@ while index < mA:
     if dbA[index] == Mx:
         index_of_max = index
     index += 1
-Rmax = index_of_max
+r_max = index_of_max
 index = 0
 
 while index < mA:
@@ -136,13 +134,13 @@ while index < mA:
         N2 = index
     index += 1
 
-L1A = frq_A_points[N1] * 10**-6
-L2A = frq_A_points[N2] * 10**-6
-CfA = (L1A+L2A)/2
-PkA = frq_A_points[Rmax]*10**-6
-dbNB = dbB[1:mB]
-Mx = max(dbNB)
-My = Mx - 6
+L1A = frq_A_points[N1] * 10**-6     # find MHz value of N1
+L2A = frq_A_points[N2] * 10**-6     # find MHz value of N2
+CfA = (L1A+L2A)/2       # find average
+PkA = frq_A_points[r_max]*10**-6        # frequency of the wave that has the highest contribution
+dbNB = dbB[1:mB]        # ignores the mean
+Mx = max(dbNB)          # max decibel of dbNB
+My = Mx - 6             # decibel threshold
 
 index = 0
 while index < mB:
@@ -164,11 +162,11 @@ CfB = (L1B+L2B)/2
 PkB = frq_A_points[index_of_max]*10**-6
 
 #  calculating transfer function
-FAf = frq_A_points[1:mA-1]
-num = fft_A[1:mA-1]
+FAf = frq_A_points[1:mA-1]  # removes zero
+num = fft_A[1:mA-1]     # removes first value bc it is a mean numerator of transfer function
 den = fft_B[1:mB]/100  # denominator of transfer function
 TNF = den/num  # output over input
-dbTNF = control.mag2db(TNF) #transfer function converted to decibels
+dbTNF = control.mag2db(TNF) # transfer function converted to decibels
 k1 = 1  # counts number of frequencies contributions below 0.5 MHz
 k2 = 1  # counts number of frequencies contributions above 3.5 MHz
 
@@ -210,5 +208,7 @@ plt.ylabel('Amplitude [dB]')
 plt.title('Transfer Func (Decibel Scale)')
 plt.grid()
 plt.show()
+
+
 
 # store transfer function data
