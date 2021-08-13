b12d54de08013e9370446c5519311003571b05da
sarpie1 <65266482+sarpie1@users.noreply.github.com>
2021-08-13 15:18:34 -0400

    Add files via upload

--- CognitiveUnit/test_cognition/refresh_Q_table.py
+++ CognitiveUnit/test_cognition/refresh_Q_table.py
@@ -5,20 +5,15 @@ def refresh_Q_table(reward_history, lxy, dia, sample_count):
 
     current_state = [lxy,dia]
     algo = CognitionAlgorithms()
+    state = current_state
     Q_table, action, current_state, next_state, reward = algo.update_Q_table(reward_history,current_state)
     
-    Qvalues = Q_table[39,53,:]
     Q_hat = np.load('Q_hat.npy')
-    Qvalues_old = Q_hat[39, 53, :]
     
-    print(f'Q_old: {Qvalues_old}')
-    print(f'Q_new: {Qvalues}')
-    # debug test 
+    # debug test: text file 
     f = open("agent_history.txt", "a")
     sample_count = int(sample_count)
-    f.write(f"s_{sample_count}:{current_state}, a_{sample_count}:{action}, s_{sample_count+1}:{next_state}, r_{sample_count}:{reward}\n")
-    f.close()
+    f.write(f"s_{sample_count}:{[lxy, dia]}, a_{sample_count}:{action}, s_{sample_count+1}:{next_state}, r_{sample_count}:{reward}\n")
+    f.flush()
     return None
 
-    
-    
