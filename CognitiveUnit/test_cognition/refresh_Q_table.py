54a22910c2958c0bbcfd729e75382335388f9535
ghost-083 <alam.92@osu.edu>
2021-08-13 16:51:51 -0400

    Q-table bug fixed

--- CognitiveUnit/test_cognition/refresh_Q_table.py
+++ CognitiveUnit/test_cognition/refresh_Q_table.py
@@ -1,4 +1,4 @@
-from test_algos import CognitionAlgorithms
+from test_algos import *
 import numpy as np
 import csv
 
@@ -8,10 +8,10 @@ def refresh_Q_table(lxy_old, dia_old, lxy_action, dia_action, lxy_new, dia_new, 
     current_state = [lxy_old, dia_old]
     next_state = [lxy_new, dia_new]
     action = [lxy_action, dia_action]
-
+    action_space = [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1],
+                         [-1, 1], [-1, -1], [1, -1], [0, 0]]
     # update Q-table
-    algo = CognitionAlgorithms()
-    Q_table, reward = algo.update_Q_table(current_state, action, next_state, reward_history)
+    Q_table, reward = update_Q_table(current_state, action, next_state, reward_history)
 
     # dump file for debugging: csv file
     csv_file = open('agent_history.csv', mode='a')
@@ -24,13 +24,24 @@ if __name__ == "__main__":
     reward_history = [99.1216]
     lxy_old, dia_old = 50, 43
     lxy_new, dia_new = 50, 43
+    action_space = [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1],
+                         [-1, 1], [-1, -1], [1, -1], [0, 0]]
 
-    for i in range(5):
+    for i in range(1):
         sample_count = i+1
-        algo = CognitionAlgorithms()
-        next_state, action = algo.Q_learning_vanilla(reward_history, lxy_old, dia_old)
+        next_state, action = implement_policy(reward_history, lxy_old, dia_old)
         lxy_new, dia_new = next_state
         lxy_action, dia_action = action
+        action_idx = action_space.index(action)
+
+        Q_table_old = np.load('Q_table.npy')
+        print(f'Q_old: {Q_table_old[lxy_old, dia_old, action_idx]}')
+
+        # update Q table
         refresh_Q_table(lxy_old, dia_old, lxy_action, dia_action, lxy_new, dia_new, reward_history, sample_count)
         reward_history.append(100 - np.random.rand())
+
+        Q_table_new = np.load('Q_table.npy')
+        print(f'Q_new: {Q_table_new[lxy_old, dia_old, action_idx]}')
+        
         lxy_old, dia_old = lxy_new, dia_new
--- CognitiveUnit/test_cognition/test_algos.py
+++ CognitiveUnit/test_cognition/test_algos.py
@@ -2,43 +2,57 @@ import numpy as np
 from policy import epsilon_greedy_policy
 
 
-class CognitionAlgorithms:
-    def __init__(self):
-        self.state_space_size = 4624
-        self.action_space_size = 9
-        self.action_space = [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1],
-                             [-1, 1], [-1, -1], [1, -1], [0, 0]]
-        self.x1_len = 68
-        self.x2_len = 68
-        self.Q_table = np.load('Q_hat.npy')
-        # ---------- hyperparameters --------
-        self.alpha = 0.5
-        self.gamma = 0.98
-        self.epsilon = 0.05
+def params():
+    state_space_size = 4624
+    action_space_size = 9
+    action_space = [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1],
+                         [-1, 1], [-1, -1], [1, -1], [0, 0]]
+    x1_len = 68
+    x2_len = 68
+    # ---------- hyperparameters --------
+    alpha = 0.5
+    gamma = 0.98
+    epsilon = 0.05
 
-    def Q_learning_vanilla(self, reward_history, lxy, dia):
-        # ----------------------------------
-        current_state = [lxy, dia]
-        if current_state[0] is None and current_state[1] is None:
-            x1, x2 = np.random.randint(0, 67), np.random.randint(0, 67)
-            current_state = [x1, x2]
-        action = epsilon_greedy_policy(self.Q_table, self.action_space, current_state, self.epsilon)
-        next_state = np.copy(current_state)
-        next_state[0] = min(max(current_state[0] + action[0], 0), 67)
-        next_state[1] = min(max(current_state[1] + action[1], 0), 67)
+    return alpha, gamma, epsilon, action_space
 
-        return next_state, action
 
-    def update_Q_table(self, current_state, action, next_state, reward_history):
-        action_index = self.action_space.index(action)
-        best_next_action_index = np.argmax(self.Q_table[next_state[0], next_state[1], :])
+def implement_policy(reward_history, lxy, dia):
+    # ----------------------------------
+    Q_table = np.load('Q_table.npy')
+    current_state = [lxy, dia]
+    alpha, gamma, epsilon, action_space = params()
 
-        reward = reward_history[-1]     # last element as reward
+    if current_state[0] is None and current_state[1] is None:
+        x1, x2 = np.random.randint(0, 67), np.random.randint(0, 67)
+        current_state = [x1, x2]
+    action = epsilon_greedy_policy(Q_table, action_space, current_state, epsilon)
+    next_state = np.copy(current_state)
+    next_state[0] = min(max(current_state[0] + action[0], 0), 67)
+    next_state[1] = min(max(current_state[1] + action[1], 0), 67)
 
-        self.Q_table[current_state[0], current_state[1], action_index] += self.alpha * (
-                reward + self.gamma * self.Q_table[next_state[0], next_state[1], best_next_action_index] -
-                self.Q_table[current_state[0], current_state[1], action_index])
+    return next_state, action
 
-        return self.Q_table, reward
+
+def update_Q_table(current_state, action, next_state, reward_history):
+    # load previous Q-table from previous timestep
+    Q_table = np.load('Q_table.npy')
+    alpha, gamma, epsilon, action_space = params()
+
+    action_index = action_space.index(action)
+    best_next_action_index = np.argmax(Q_table[next_state[0], next_state[1], :])
+
+    reward = reward_history[-1]     # last element as reward
+
+    print(Q_table[current_state[0], current_state[1], action_index])
+    Q_table[current_state[0], current_state[1], action_index] += alpha * (
+            reward + gamma * Q_table[next_state[0], next_state[1], best_next_action_index] -
+            Q_table[current_state[0], current_state[1], action_index])
+
+    # save updated Q-table
+    np.save('Q_table.npy', Q_table)
+    print(Q_table[current_state[0], current_state[1], action_index])
+
+    return Q_table, reward
 
 
