79a43586959daca2b499b548dc78b224a446d93a
ferdous-alam <42524560+ferdous-alam@users.noreply.github.com>
2022-03-07 00:54:04 -0500

    delete PRM-TAPRL old version

--- CognitiveUnit/Brain_PRL-TAPRL/temp.py
+++
@@ -1,7 +0,0 @@
-
-
-# append to log file for experiment details
-log_file = open("dump/experiment_no_{}_details.txt".format(exp_num), "a")
-details = 'This is a test \n'
-log_file.write(details)
-log_file.close()
